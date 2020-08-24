e634c37cbccbb94672b0be5549977c7b85aadffe
Steve Emmanuel <steve.emmanuel16@hotmail.com>
2020-08-23 23:47:56 -0400

    Flask blog work

---
+++ site_server.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+posts = [
+    {
+        'author': 'Steve Emmanuel',
+        'title': 'Blog one',
+        'content': 'First blog post',
+        'date_posted': 'July 18, 2020',
+    },
+    {
+        'author': 'Damian Emmanuel',
+        'title': 'Blog two',
+        'content': 'Second blog post',
+        'date_posted': 'July 17, 2020',
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/test')
+def test():
+    return 'This is the login page.'
+
